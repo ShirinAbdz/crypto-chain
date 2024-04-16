97ad20accd457e04ef80dfc27e9ff130aeaac4fb
Shirin.abdz <shiringohariii@gmail.com>
2024-04-16 19:21:20 +0430

    Bug fixed on pagination

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -2,7 +2,7 @@ const BASE_URL = "https://api.coingecko.com/api/v3";
 const API_KEY = "CG-p6T3Gzr7wrbjGc3i2xPRAErg";
 
 const getCoinList = (page, currency) => {
-  return `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=60&page=${page}&sparkline=false&locale=en&x_cg_demo_api_key=${API_KEY}`;
+  return `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=30&page=${page}&sparkline=false&locale=en&x_cg_demo_api_key=${API_KEY}`;
 };
 const searchCoin = (query) => {
   return `${BASE_URL}/search?query=${query}&x_cg_demo_api_key=${API_KEY}`;
